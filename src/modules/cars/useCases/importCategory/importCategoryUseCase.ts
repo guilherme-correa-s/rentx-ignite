56660878320ade0830b496b6540eda11e812641d
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2021-11-08 23:05:33 -0300

    feat(categories): added csv parse for import categories

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,5 +1,53 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+
+interface IImportCaregory {
+  name: string;
+  description: string;
+}
+
 export class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    console.log(file);
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCaregory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCaregory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const categoryNameAlreadyExists =
+        this.categoriesRepository.findByName(name);
+
+      if (!categoryNameAlreadyExists) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
   }
 }
