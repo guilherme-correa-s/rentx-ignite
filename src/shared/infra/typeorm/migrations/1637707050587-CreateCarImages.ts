68dddfaed0b0d47bea78ba5b0c373a829b864f44
guilherme-correa-s <guilhermescorrea@hotmail.com.br>
2021-11-27 23:05:00 -0300

    chore(typeorm): migrations

---
+++ src/shared/infra/typeorm/migrations/1637707050587-CreateCarImages.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCarImages1637707050587 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars_image',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'image_name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCarImage',
+            referencedTableName: 'cars',
+            referencedColumnNames: ['id'],
+            columnNames: ['car_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars_image');
+  }
+}
